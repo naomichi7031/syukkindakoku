de2fedb873f9f06b2a0361645f53b2fbc498e089
naomichi7031 <veg.j.qh@gmail.com>
2026-07-01 22:15:13 +0900

    attendance.py

--- attendance.py
+++ attendance.py
@@ -1,15 +1,40 @@
-from playwright.sync_api import sync_playwright
 import os
+from playwright.sync_api import Playwright, sync_playwright
 
-ID = os.environ.get("LOGIN_ID")
-PASS = os.environ.get("LOGIN_PASSWORD")
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True)
-    page = browser.new_page()
+USER_ID = os.getenv("ATTENDANCE_ID")
+PASSWORD = os.getenv("ATTENDANCE_PASSWORD")
 
-    page.goto("https://sa.wims.jp/n9oZ2s/WA014Init.do?compUrl=OMK3")
 
-    print("ログイン画面を開きました")
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=True)
 
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://sa.wims.jp/n9oZ2s/WP001Init.do?compUrl=OMK3")
+
+    # ID入力
+    page.get_by_role("textbox", name="User ID").fill(USER_ID)
+
+    # パスワード入力
+    page.get_by_role("textbox", name="Password").fill(PASSWORD)
+
+    # ログインボタン押下
+    page.get_by_role("button").click()
+
+    page.wait_for_timeout(3000)
+
+    # 出勤押下
+    page.get_by_role("link", name="出勤").click()
+
+    print("出勤打刻完了")
+
+    page.wait_for_timeout(3000)
+
+    context.close()
     browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
