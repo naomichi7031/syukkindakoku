5a80c00ceea1d2f7740643e8922e7bcda7628593
naomichi7031 <veg.j.qh@gmail.com>
2026-07-03 19:45:36 +0900

    Refactor attendance.py for improved browser handling
    Refactor attendance.py to simplify browser handling and remove unnecessary context creation.

--- attendance.py
+++ attendance.py
@@ -1,39 +1,23 @@
 import os
-from playwright.sync_api import Playwright, sync_playwright
-
+from playwright.sync_api import sync_playwright
 
 USER_ID = os.getenv("ATTENDANCE_ID")
 PASSWORD = os.getenv("ATTENDANCE_PASSWORD")
 
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=True)
-
-    context = browser.new_context()
-    page = context.new_page()
+    page = browser.new_page()
 
     page.goto("https://sa.wims.jp/n9oZ2s/WP001Init.do?compUrl=OMK3")
 
     print("ページを開きました")
 
-    # ID入力
     page.get_by_role("textbox", name="User ID").fill(USER_ID)
-
-    # パスワード入力
     page.get_by_role("textbox", name="Password").fill(PASSWORD)
 
-    print("ID/PASS入力完了")
-
-    # 出勤ボタン押下
     page.get_by_role("link", name="出勤").click()
 
-    print("出勤打刻完了")
+    print("打刻完了")
 
-    page.wait_for_timeout(5000)
-
-    context.close()
     browser.close()
-
-
-with sync_playwright() as playwright:
-    run(playwright)
